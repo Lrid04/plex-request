648e17bf2163c9b5ad9d11f2738b8cb3604ee20f
Lrid04 <landon.riddik04@gmail.com>
2024-12-01 17:33:28 -0600

    basic json loading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,27 @@
-import Image from "next/image";
+'use client'
+import { useState, useEffect } from 'react'
+import data from "./data/movies.json"
+
+type Movie = {
+  movieId: number,
+  movieName: string,
+  releaseYear: number,
+  posterUrl: string
+}
 
 export default function Home() {
+  const [movies, setMovies] = useState<Movie[]>([]);
+
+  useEffect(() => {
+    setMovies(data);
+  }, []);
+  
+  movies.map(
+    movie => {
+      console.log(movie)
+    }
+  )
+  console.log(movies)
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       
